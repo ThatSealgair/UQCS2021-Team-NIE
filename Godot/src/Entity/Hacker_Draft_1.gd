0ef4e8c48c6377c2410c9fd0b8a4cc2b4bffcb81
Tze Chian Kam <tze.chian.kam@gmail.com>
2021-09-12 12:03:35 +1000

    Editted Hacker script.

--- Godot/src/Entity/Hacker_Draft_1.gd
+++ Godot/src/Entity/Hacker_Draft_1.gd
@@ -2,31 +2,32 @@ extends Area2D
 
 class_name Hacker
 
-var needs_dict = {} # TODO: Add needs for Hacker.
+var needs_dict = {0: 'Pizza', 1: 'Red Bull', 2: 'Coffee'} # TODO: Add needs for Hacker.
 var item_need_no = RandomNumberGenerator.new()
 var need_timer_spawn = RandomNumberGenerator.new()
 var leave = false
 var rng_needs_index : int
 var rng_need_spawn : int
 var hacker_need : String
-onready var timer1 = get_node("Timer1")
-onready var timer2 = get_node("Timer2")
+onready var need_timer = get_node("Need_Timer")
+onready var want_timer = get_node("Want_Timer")
 
 func _ready():
 	need_timer_spawn.randomize()
 	rng_need_spawn = need_timer_spawn.randi_range(1, 10)
-	timer2.set_wait_time(rng_need_spawn)
-	timer2.start()
+	want_timer.set_wait_time(rng_need_spawn)
+	want_timer.start()
 	
 func _wants():
 	item_need_no.randomize()
-	rng_needs_index = item_need_no.randi_range(0, 3)
+	rng_needs_index = item_need_no.randi_range(0, 2)
 	hacker_need = needs_dict[rng_needs_index]
-	timer1.set_wait_time(5)
-	timer1.start()
+	# Add integration to player.
+	need_timer.set_wait_time(5)
+	need_timer.start()
 
 func _on_Timer_timeout():
 	leave = true
 
-func _on_Timer2_timeout():
+func _on_want_timer_timeout():
 	_wants()
