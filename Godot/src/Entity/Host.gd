51270ec5853dd5477d724c2d197dc20dd4670a51
Tze Chian Kam <tze.chian.kam@gmail.com>
2021-09-12 11:49:12 +1000

    Testing host movement.

--- Godot/src/Entity/Host.gd
+++ Godot/src/Entity/Host.gd
@@ -7,7 +7,7 @@ var _2d_velocity : = Vector2.ZERO
 func _physics_process(delta: float) -> void:
 	var direction : = Vector2(
 		Input.get_action_strength("right") - Input.get_action_strength("left"),
-		Input.get_action_strength("up") - Input.get_action_strength("down")
+		Input.get_action_strength("down") - Input.get_action_strength("up")
 	)
 	_2d_velocity= linear_speed * direction
 	_2d_velocity = move_and_slide(_2d_velocity)
